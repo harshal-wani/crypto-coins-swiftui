58bd8fde7b18db52d41c06d9c8c4909bb7227455
Harshal Wani <harshal.wani@live.com>
2025-01-03 20:19:38 +0530

    Project setup

---
+++ crypto-coins-swiftui/crypto-coins-swiftui/crypto_coins_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_coins_swiftuiApp.swift
+//  crypto-coins-swiftui
+//
+//  Created by Harshal Wani on 03/01/25.
+//
+
+import SwiftUI
+
+@main
+struct crypto_coins_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
