962553b88b2b88ceaa49a5c2ff184a840c42f155
Harshal Wani <harshal.wani@live.com>
2025-01-15 09:58:32 +0530

    Refactor view model and view

--- crypto-coins-swiftui/crypto-coins-swiftui/CryptoList/ViewModel/CryptoViewModel.swift
+++ crypto-coins-swiftui/crypto-coins-swiftui/CryptoList/ViewModel/CryptoViewModel.swift
@@ -23,32 +23,26 @@ final class CryptoViewModel: CryptoListListener, ObservableObject {
   
   // MARK: - Dependencies
   private let dataProvider: CryptoDataProviding
-//  private let router: CryptoRouteable
+  private var cancellables = Set<AnyCancellable>()
   
   // MARK: - Internal State
-  private var cryptoCoins: [CryptoItem] = []
-  private var filterSet: [CryptoFilterSet] = []
+  private var cryptoCoins = [CryptoItem]()
+  private var filterSet = [CryptoFilterSet]()
   
-  @Published var filterTags: [TagViewItem] = []
-  @Published private(set) var filteredCoins: [CryptoItem] = []
+  @Published var filterTags = [TagViewItem]()
+  @Published private(set) var filteredCoins = [CryptoItem]()
   @Published private(set) var dataState: DataState = .loading
-  @Published var searchPhrase: String = ""
-
-  
-  private var cancellables: Set<AnyCancellable> = []
+  @Published var searchPhrase = ""
   
   // MARK: - Initialization
   init(dataProvider: CryptoDataProviding) {
     self.dataProvider = dataProvider
-//    self.router = router
-    fetchAndInitializeData()
-    filterTags = CryptoFilterSet.allCases.map { TagViewItem(text: $0.text) }
-    bindCoinsPublisher()
-    bindSearchPhrasePublisher()
+    setupFilterTags()
+    bindPublishers()
   }
   
   // MARK: - Data Fetching
-  func fetchAndInitializeData() {
+  func initializeData() {
     dataState = .loading
     Task {
       do {
@@ -64,13 +58,18 @@ final class CryptoViewModel: CryptoListListener, ObservableObject {
     }
   }
   
+  // MARK: - Binding Publishers
+  private func bindPublishers() {
+    bindCoinsPublisher()
+    bindSearchPhrasePublisher()
+  }
+  
   private func bindCoinsPublisher() {
     dataProvider.coinsPublisher
+      .receive(on: DispatchQueue.main)
       .sink { [weak self] coins in
         self?.cryptoCoins = coins
-        DispatchQueue.main.async {
-          self?.filteredCoins = coins
-        }
+        self?.filteredCoins = coins
       }
       .store(in: &cancellables)
   }
@@ -85,7 +84,6 @@ final class CryptoViewModel: CryptoListListener, ObservableObject {
       .store(in: &cancellables)
   }
   
-
   // MARK: - Public Methods
   func search(for searchPhrase: String) {
     self.searchPhrase = searchPhrase
@@ -101,32 +99,33 @@ final class CryptoViewModel: CryptoListListener, ObservableObject {
   }
   
   func didTapOnCoin(_ cryptoItem: CryptoItem) {
-//    router.routeToDetail(for: cryptoItem)
+    // Implement routing logic here if needed
   }
   
   // MARK: - Private Methods
+  private func setupFilterTags() {
+    filterTags = CryptoFilterSet.allCases.map { TagViewItem(text: $0.text) }
+  }
   
   private func filterCoinsBySearchPhrase(_ searchPhrase: String) -> [CryptoItem] {
     cryptoCoins.filter { $0.name.range(of: searchPhrase, options: .caseInsensitive) != nil }
   }
   
   private func updateFilterSet(_ filter: String, action: FilterAction) {
-      if let filterItem = CryptoFilterSet.allCases.first(where: { $0.text == filter }) {
-          switch action {
-          case .add:
-              if !filterSet.contains(filterItem) {
-                  filterSet.append(filterItem)
-              }
-          case .remove:
-              filterSet.removeAll { $0 == filterItem }
-          }
+    if let filterItem = CryptoFilterSet.allCases.first(where: { $0.text == filter }) {
+      switch action {
+      case .add:
+        if !filterSet.contains(filterItem) {
+          filterSet.append(filterItem)
+        }
+      case .remove:
+        filterSet.removeAll { $0 == filterItem }
       }
+    }
   }
-
   
   private func applyFilters(searchPhrase: String) {
     let filteredBySearch = searchPhrase.isEmpty ? cryptoCoins : filterCoinsBySearchPhrase(searchPhrase)
-    
     filteredCoins = filteredBySearch.filter { coin in
       filterSet.allSatisfy { $0.matches(item: coin) }
     }
--- crypto-coins-swiftui/crypto-coins-swiftui/CryptoList/Views/CryptoListView.swift
+++ crypto-coins-swiftui/crypto-coins-swiftui/CryptoList/Views/CryptoListView.swift
@@ -16,47 +16,50 @@ struct CryptoListView: View {
     _viewModel = StateObject(wrappedValue: viewModel)
   }
   
-  var filterTagsView: [SwiftUICustomTagView<TagsView>] {
-    viewModel.filterTags.indices.map { item in
-      SwiftUICustomTagView(content: {
-        TagsView(data: $viewModel.filterTags[item]) { tag, action in
+  private var filterTagsView: [SwiftUICustomTagView<TagsView>] {
+    viewModel.filterTags.indices.map { index in
+      SwiftUICustomTagView {
+        TagsView(data: $viewModel.filterTags[index]) { tag, action in
           handleTagTap(tag, action: action)
         }
-      })
+      }
     }
   }
   
   var body: some View {
     NavigationStack {
       VStack {
+        Spacer()
         SwiftUICustomTagListView(filterTagsView, horizontalSpace: 8, verticalSpace: 8)
           .padding(.leading, 16)
           .padding(.top, 16)
-          .frame(minHeight: 50) // Minimum height for the tag list view
-          .fixedSize(horizontal: false, vertical: true)
-        List {
-          ForEach(viewModel.filteredCoins, id: \.self) { item in
-            CryptoItemRow(cryptoItem: item)
-          }
+          .padding(.bottom, -30)
+        List(viewModel.filteredCoins, id: \.self) { item in
+          CryptoItemRow(cryptoItem: item)
         }
       }
-      
       .navigationTitle("Crypto List")
       .onAppear {
-        viewModel.fetchAndInitializeData()
+        viewModel.initializeData()
       }
     }
     .searchable(text: $viewModel.searchPhrase)
   }
   
   private func handleTagTap(_ text: String, action: FilterAction) {
-    print("text: \(text), action: \(action)")
-      viewModel.filterCoins(with: text, action: action, searchPhrase: viewModel.searchPhrase)
+    viewModel.filterCoins(with: text, action: action, searchPhrase: viewModel.searchPhrase)
+  }
+}
+
+struct ViewHeightKey: PreferenceKey {
+  static var defaultValue: CGFloat = 0
+  static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+    value = nextValue()
   }
 }
 
 #Preview {
-  let dp = CryptoDataProvider(apiService: APIService.shared)
-  let vm = CryptoViewModel(dataProvider: dp)
-  CryptoListView(viewModel: vm)
+  let dataProvider = CryptoDataProvider(apiService: APIService.shared)
+  let viewModel = CryptoViewModel(dataProvider: dataProvider)
+  CryptoListView(viewModel: viewModel)
 }
